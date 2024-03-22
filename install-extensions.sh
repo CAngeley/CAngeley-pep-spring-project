15d2ee96e31b0783a3be4f6ef839db09374194d3
Connie Angeley <54635759+CAngeley@users.noreply.github.com>
2024-03-22 12:33:16 -0500

    File Commit From RevPro

---
+++ install-extensions.sh
@@ -0,0 +1,2 @@
+code --install-extension redhat.java@1.22.1 && code --install-extension vscjava.vscode-java-debug sleep 40 && code --install-extension revaturePro.revature-labs
+for i in {1..3}; do code --uninstall-extension vscjava.vscode-java-test; sleep 15; done
